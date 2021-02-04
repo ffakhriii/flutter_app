5aa663a846088f957f21b05798d11c2a932d98e4
fakhri <fakhrimoo19@gmail.com>
2021-02-04 21:23:47 +0700

    pertama

--- lib/Hal4.dart
+++ lib/Hal4.dart
@@ -4,41 +4,137 @@ import 'package:flutter/material.dart';
 class Hal4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var mediaQueryData = MediaQuery.of(context);
+    final double widthScreen = mediaQueryData.size.width;
+    final double appBarHeight = kToolbarHeight;
+    final double paddingTop = mediaQueryData.padding.top;
+    final double paddingBottom = mediaQueryData.padding.bottom;
+    final double heightScreen = mediaQueryData.size.height -
+        paddingBottom -
+        paddingTop -
+        appBarHeight -
+        50.0;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "GridView",
           style: TextStyle(color: Colors.white),
         ),
+        centerTitle: true,
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: <Widget>[
-          Container(
-            color: Colors.yellowAccent,
-            child: Center(
-              child: Text("1", style: TextStyle(fontSize: 24.0),),
+      body: SafeArea(
+        child: GridView.count(
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          childAspectRatio: widthScreen / heightScreen,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20.0),
+                    ),
+                    color: Colors.yellow),
+                child: Center(
+                  child: Text(
+                    "1",
+                    style:
+                    TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
             ),
-          ),
-          Container(
-            color: Colors.blueAccent,
-            child: Center(
-              child: Text("2", style: TextStyle(fontSize: 24.0),),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20.0),
+                    ),
+                    color: Colors.red),
+                child: Center(
+                  child: Text(
+                    "2",
+                    style:
+                    TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
             ),
-          ),
-          Container(
-            color: Colors.brown,
-            child: Center(
-              child: Text("3", style: TextStyle(fontSize: 24.0),),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20.0),
+                    ),
+                    color: Colors.blue),
+                child: Center(
+                  child: Text(
+                    "3",
+                    style:
+                    TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
             ),
-          ),
-          Container(
-            color: Colors.orange,
-            child: Center(
-              child: Text("4", style: TextStyle(fontSize: 24.0),),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20.0),
+                    ),
+                    color: Colors.green),
+                child: Center(
+                  child: Text(
+                    "4",
+                    style:
+                    TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20.0),
+                    ),
+                    color: Colors.teal),
+                child: Center(
+                  child: Text(
+                    "5",
+                    style:
+                    TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20.0),
+                    ),
+                    color: Colors.deepOrange),
+                child: Center(
+                  child: Text(
+                    "6",
+                    style:
+                    TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
