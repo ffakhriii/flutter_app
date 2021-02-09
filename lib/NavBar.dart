365e53f599d1560d68e9f95c9df788e62956ae70
fakhri <fakhrimoo19@gmail.com>
2021-02-09 21:53:09 +0700

    push

--- lib/NavBar.dart
+++ lib/NavBar.dart
@@ -2,6 +2,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/Hal1.dart';
 import 'package:flutter_app/Hal2.dart';
+import 'package:flutter_app/Hal7.dart';
 import 'package:flutter_app/hal3.dart';
 import 'package:flutter_app/hal4.dart';
 import 'package:flutter_app/Hal5.dart';
@@ -15,7 +16,7 @@ class NavBar extends StatefulWidget {
 class _NavBarState extends State<NavBar> {
   int selectedpage = 0;
 
-  final _halaman = [Hal1(),Hal2(),Hal3(),Hal4(),Hal5(),Hal6()]; //listing of all 6 page
+  final _halaman = [Hal1(),Hal2(),Hal3(),Hal4(),Hal5(),Hal6(),Hal7()]; //listing of all 6 page
 
   final bgcolor = [Colors.green,Colors.red,Colors.blue,Colors.yellow,Colors.orange,Colors.indigo]; //color changing
 
